88cc89e199a19ca496570c9f22af9d7f8a179971
leider <derleider@web.de>
2012-11-26 23:21:56 +0100

    code and fitnesse page

---
+++ SpecByExample/HotDog/src/hotdog/automatischebestellung/Bestellschluss.java
@@ -0,0 +1,11 @@
+package hotdog.automatischebestellung;
+
+import fit.ColumnFixture;
+
+public class Bestellschluss extends ColumnFixture {
+
+	public String bestellschluss() {
+		return "16:00";
+	}
+	
+}
